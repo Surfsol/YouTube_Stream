ec3e3f23fb981e75131f6829ee80612590bda150
Surfsol <surfsol@protonmail.com>
2021-09-16 19:15:26 -0300

    added navigation, VideoScreen, HomeScreen.js

---
+++ screens/VideoScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, StyleSheet, View, Text } from 'react-native';
+
+function VideoScreen({ navigation }) {
+    return (
+    <View>
+      <Text>Video Screen</Text>
+    </View>
+    );
+  }
+  
+  export default VideoScreen
