c233810e829c3551059b395e173aa8d491128d24
Surfsol <surfsol@protonmail.com>
2021-09-28 22:10:06 -0300

    add tsx

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import {
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  TextInput,
+  NativeModules,
+} from 'react-native';
+import {wp} from '../utils/dimensions';
+
+const {CustomNative} = NativeModules;
+
+function HomeScreen({navigation}) {
+  const [search, setSearch] = useState();
+  const [adjustedSearch, setAdjustedSearch] = useState();
+
+  const replaceWhitespace = string => {
+    const adjusted = string.replace(/\s/g, '%20');
+    return adjusted;
+  };
+
+  const saveSearch = search => {
+    const adjustedSearch = replaceWhitespace(search);
+    setAdjustedSearch(adjustedSearch);
+    setSearch(search);
+  };
+
+  const kotlinMessage = () => {
+    CustomNative.Toastshow(`Searching for ${search}`, CustomNative.LONG);
+  };
+
+  return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>YouTube Live Streaming</Text>
+
+        <TextInput
+          style={styles.input}
+          onChangeText={search => saveSearch(search)}
+          value={search}
+          placeholder="Search"
+        />
+        <Button
+          title="Search"
+          onPress={() => {
+            kotlinMessage();
+            navigation.navigate('Videos', {adjustedSearch: adjustedSearch});
+          }}
+        />
+    </SafeAreaView>
+  );
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#252324',
+  },
+  title: {marginBottom: 60, fontSize: 30, color: '#FF0000', fontWeight: 'bold'},
+  input: {
+    height: 40,
+    width: wp(60),
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+});
