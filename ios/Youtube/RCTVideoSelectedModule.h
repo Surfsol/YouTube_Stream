18d3b63957e76df25f2f305f704078587473cfce
Surfsol <surfsol@protonmail.com>
2021-09-30 10:21:42 -0300

    add ios module

---
+++ ios/Youtube/RCTVideoSelectedModule.h
@@ -0,0 +1,6 @@
+//  RCTCalendarModule.h
+// React/RCTBridgeModule.h needs to be started in Xcode
+// When you create a new method, put the declaration in .h file
+#import <React/RCTBridgeModule.h>
+@interface RCTVideoSelectedModule : NSObject <RCTBridgeModule>
+@end
